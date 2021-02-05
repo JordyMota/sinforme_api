d1b1a3a3fff0583aa93de032906a20459621d335
Jordy Mota <42422126+JordyMota@users.noreply.github.com>
2021-02-05 09:22:25 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    EMAIL: "jordymota@osinforme.com.br",
-    PASSWORD: "5@5Ci3cs6C@Sr_R",
-    // MAIN_URL: "http://osinforme.com.br/", 
-    MAIN_URL: "http://localhost:3000/", 
+    EMAIL: "**sinforme_email**",
+    PASSWORD: "**sinforme_password**",
+    MAIN_URL: "http://osinforme.com.br/", 
+    // MAIN_URL: "http://localhost:3000/", 
 };
